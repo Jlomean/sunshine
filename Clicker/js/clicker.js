4cd88bc9022e9226d9350e9f889c71510a4806b9
Laaria <gervaisroxane@gmail.com>
2017-07-22 14:15:48 +0200

    Last step done - styling in progress

--- Clicker/js/clicker.js
+++ Clicker/js/clicker.js
@@ -1,46 +1,67 @@
-h = document.getElementById("display");
-console.log(h);
 
-b = document.getElementById("sun");
-console.log(b);
-
-var score = 0;
-var multiplier = 1;
+var score = 0; //Initial score 
+var multiplier = 1; //Initial multiplier
+var bonusValue = 1; //Bonus value defines wether or not score is doubled by bonusScore function
 
 function clickButton() {
-	score += multiplier ;
-	document.getElementById("display").innerHTML = "Chaleur : " + score + " ° K";
+	score += multiplier*bonusValue ;
+	document.getElementById("display").innerHTML = score + " candela";
+	hideButtons();
 }
 
 function augmenterMultiplicateur() {
-	if (score >= (10*multiplier)) {
-		score -= (10*multiplier);	
-		multiplier++;
-		document.getElementById("display").innerHTML = "Chaleur : " + score + " ° K";
-		document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier + " | Acheter pour " + 10*multiplier + " ° K";
-	}
-	else {
-		document.getElementById("multiply").innerHTML = "  Il fait trop froid !  ";
-		setInterval(function(){
-			document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier + " | Acheter pour " + 10*multiplier + " ° K";
-		}, 3000);		
-	}
+	score -= (50*multiplier);	
+	multiplier++;
+	document.getElementById("display").innerHTML = score + " candela";
+	document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier + " | Acheter pour " + 50*multiplier + " candela";
+	hideButtons();
 }
 
 function autoClick() {
-	if (score >= 500) {
-		score -= 500;
-		document.getElementById("autoclic").disabled = true;
-		document.getElementById("autoclic").innerHTML = "Autoclic : on";
-		setInterval(function(){
-			document.getElementById("sun").click();
-		}, 1000);
-		document.getElementById("display").innerHTML = "Chaleur : " + score + " ° K";
+	score -= 500;
+	document.getElementById("autoclic").style.visibility= "collapse";
+	document.getElementById("boughtauto").style.visibility= "visible"; // Show with css that autoclic is bought now.
+	setInterval(function(){
+		document.getElementById("sun").click();
+	}, 1000);
+	document.getElementById("display").innerHTML = score + " candela";
+	hideButtons();
+}
+
+function bonusScore() {
+	score -= 5000;
+	document.getElementById("bonus").style.visibility= "collapse";
+	document.getElementById("special").style.visibility= "visible"; // Additionnal visual effect for special timed events. Usable for future events.
+	document.getElementById("special").innerHTML = "Fiat lux !";
+	bonusValue = bonusValue*2;
+	hideButtons();
+	
+	setTimeout(function(){
+		bonusValue = bonusValue/2;
+		document.getElementById("bonus").style.visibility= "visible";
+		document.getElementById("special").style.visibility= "hidden";
+		document.getElementById("bonus").innerHTML = "Fiat lux : off | Acheter pour 5000 candela";			
+	}, 30000);
+	clearInterval();
+}
+
+function hideButtons() {
+	if (score >= (50*multiplier)) {
+		document.getElementById("multiply").disabled = false;
 	}
 	else {
-		document.getElementById("autoclic").innerHTML = "  Il fait trop froid !  ";
-		setInterval(function(){
-			document.getElementById("autoclic").innerHTML = "autoclic : off | Acheter pour 500 ° K";
-		}, 3000);
+		document.getElementById("multiply").disabled = true;
+	}
+	if (score >= 500) {
+		document.getElementById("autoclic").disabled = false;
+	}
+	else {
+		document.getElementById("autoclic").disabled = true;
+	}
+	if (score >= 5000) {
+		document.getElementById("bonus").disabled = false;
+	}
+	else {
+		document.getElementById("bonus").disabled = true;
 	}
 }
