f9e349e4d90f6664e78557fa51122fc1f4511638
Laaria <gervaisroxane@gmail.com>
2017-07-22 10:07:30 +0200

    Step 12 done

--- Clicker/js/clicker.js
+++ Clicker/js/clicker.js
@@ -13,13 +13,34 @@ function clickButton() {
 }
 
 function augmenterMultiplicateur() {
-	if (score >= 50) {	
+	if (score >= (10*multiplier)) {
+		score -= (10*multiplier);	
 		multiplier++;
-		score -= 50;
 		document.getElementById("display").innerHTML = "Chaleur : " + score + " ° K";
-		document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier +" | Acheter (prix : 50 ° K)";
+		document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier + " | Acheter pour " + 10*multiplier + " ° K";
 	}
 	else {
-		document.getElementById("multiply").innerHTML = " Vous n'avez pas assez de fonds !";
+		document.getElementById("multiply").innerHTML = "  Il fait trop froid !  ";
+		setInterval(function(){
+			document.getElementById("multiply").innerHTML = "Multiplicateur : " + multiplier + " | Acheter pour " + 10*multiplier + " ° K";
+		}, 3000);		
+	}
+}
+
+function autoClick() {
+	if (score >= 500) {
+		score -= 500;
+		document.getElementById("autoclic").disabled = true;
+		document.getElementById("autoclic").innerHTML = "Autoclic : on";
+		setInterval(function(){
+			document.getElementById("sun").click();
+		}, 1000);
+		document.getElementById("display").innerHTML = "Chaleur : " + score + " ° K";
+	}
+	else {
+		document.getElementById("autoclic").innerHTML = "  Il fait trop froid !  ";
+		setInterval(function(){
+			document.getElementById("autoclic").innerHTML = "autoclic : off | Acheter pour 500 ° K";
+		}, 3000);
 	}
 }
